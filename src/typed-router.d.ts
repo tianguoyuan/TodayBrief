be4b522c147eced1314bb239a956491ade37569f
Anthony Fu <github@antfu.me>
2026-09-08 14:32:06 +0900

    chore: update deps

--- src/typed-router.d.ts
+++ src/typed-router.d.ts
@@ -14,10 +14,15 @@ import type {
   ParamValueZeroOrMore,
   ParamValueZeroOrOne,
 } from 'vue-router'
+import type {
+  _ExtractParamParserType,
+} from 'vue-router/experimental'
 
 declare module 'vue-router' {
   interface TypesConfig {
-    ParamParsers: never
+    _ParamParsers: {}
+    RouteNamedMap: import('vue-router/auto-routes').RouteNamedMap
+    _RouteFileInfoMap: import('vue-router/auto-routes')._RouteFileInfoMap
   }
 }
 
@@ -65,18 +70,24 @@ declare module 'vue-router/auto-routes' {
         | '/'
       views:
         | never
+      pathParamNames:
+        | never
     }
     'src/pages/[...all].vue': {
       routes:
         | '/[...all]'
       views:
         | never
+      pathParamNames:
+        | 'all'
     }
     'src/pages/hi/[name].vue': {
       routes:
         | '/hi/[name]'
       views:
         | never
+      pathParamNames:
+        | 'name'
     }
   }
 
